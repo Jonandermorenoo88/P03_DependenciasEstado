40aef124ef7875a4da5f000798d1f31615462c04
Adrian Zamora <adrian.zs.19042002@gmail.com>
2024-03-16 19:12:53 +0100

    Se añaden comentarios de la interfaz del parque

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -1,8 +1,25 @@
 package p03.c01;
 
+/**
+ * Interfaz del parque, implementa de forma abstracta los métodos  
+ * de entrada y salida del parque
+ * 
+ * @author Adrián Zamora Sánchez
+ * @author Jon Ander Incera Moreno
+ */
 public interface IParque {
-	
+	/**
+	 * Método de entrada al parque
+	 * 
+	 * @param puerta id de la puerta
+	 */
 	public abstract void entrarAlParque(String puerta);
+	
+	/**
+	 * Método de salida del parque
+	 * 
+	 * @param puerta id de la puerta
+	 */
 	public abstract void salirDelParque(String puerta);
 
 }
