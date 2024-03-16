95f56f232d750616ce73bf0910d8ce08f6afe22c
Adrian Zamora <adrian.zs.19042002@gmail.com>
2024-03-16 12:10:42 +0100

    Se añade Actividad Salida Puerta

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,5 +1,35 @@
 package p03.c01;
 
-public class ActividadSalidaPuerta {
-	// TODO
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Adrián Zamor Sánchez
+ * @author Jon Ander Incera Moreno
+ */
+public class ActividadSalidaPuerta implements Runnable {
+
+	// estos son los atributos
+	private static final int NUMENTRADAS = 20;
+	private String puerta;
+	private IParque parque;
+
+	public ActividadSalidaPuerta(String puerta, IParque parque) {
+		this.puerta = puerta;
+		this.parque = parque;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < NUMENTRADAS; i++) {
+			try {
+				parque.salirDelParque(puerta);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5) * 500);
+			} catch (InterruptedException e) {
+				System.out.println("Salida interrumpida " + puerta);
+				System.out.println(e.toString());
+				return;
+			}
+		}
+	}
 }
--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -3,6 +3,6 @@ package p03.c01;
 public interface IParque {
 	
 	public abstract void entrarAlParque(String puerta);
-	public abstract void salirAlParque(String puerta);
+	public abstract void salirDelParque(String puerta);
 
 }
