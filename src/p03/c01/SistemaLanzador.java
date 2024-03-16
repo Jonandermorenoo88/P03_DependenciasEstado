102a0360632dd43eb3a5a26e2dcf9ad0073743f6
Adrian Zamora <adrian.zs.19042002@gmail.com>
2024-03-16 12:43:55 +0100

    Se implementa el metodo main de SistemaLanzador

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -1,5 +1,8 @@
 package p03.c01;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 /**
  * @author Adrián Zamora Sánchez
  * @author Jon Ander Incera Moreno
@@ -7,6 +10,19 @@ package p03.c01;
 public class SistemaLanzador {
 
 	public static void main(String[] args) {
+
+		IParque parque = AdaptadorParqueSincronizado.getInstancia();
+		char letra_puerta = 'A';
+
+		Executor exec = Executors.newCachedThreadPool();
 		
+		for (int i = 0; i < Integer.parseInt(args[0]); i++) { // Bucle que recoge las entradas por argumento
+
+			String puerta = "" + ((char) (letra_puerta++));
+
+			exec.execute(new ActividadEntradaPuerta(puerta, parque));
+			exec.execute(new ActividadSalidaPuerta(puerta, parque));
+			
+		}
 	}
 }
