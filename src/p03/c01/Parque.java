51fe7560f68c70d7e7ad87c40d396f4edae01f20
Adrian Zamora <adrian.zs.19042002@gmail.com>
2024-03-16 12:24:00 +0100

    Se añaden los metodos abstractos de Parque, se añade un bucle para lanzar hilos en SistemaLanzador y se añaden comentarios

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -1,8 +1,20 @@
 package p03.c01;
 
-public class Parque{
+public class Parque implements IParque {
 
 	public Parque() {
+		
+	}
+
+	@Override
+	public void entrarAlParque(String puerta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void salirDelParque(String puerta) {
+		// TODO Auto-generated method stub
 		
 	}
 }
