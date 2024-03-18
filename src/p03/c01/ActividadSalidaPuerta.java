24ab867ea90b1dadd54bc49d053db97fdacb35bd
jonandermorenoo <118752596+jonandermorenoo@users.noreply.github.com>
2024-03-18 12:55:12 +0100

    agregando comentarios de la clase ActividadSalidaPuerta

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -4,6 +4,11 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * Clase Actividad salida Puerta. Esta clase representa una actividad de salida
+ * a traves de una puerta en un parque. imprementa la interfaz runnable para
+ * poder ser ejecutada en un hilo de ejecución.
+ * 
+ * 
  * @author Adrián Zamor Sánchez
  * @author Jon Ander Incera Moreno
  */
@@ -14,11 +19,27 @@ public class ActividadSalidaPuerta implements Runnable {
 	private String puerta;
 	private IParque parque;
 
+	/**
+	 * contructor de la clase ActividadSalidaPuerta. Inicializa los atributos puerta
+	 * y parque con los valores especificos en los parametros.
+	 * 
+	 * @param puerta una cadena que representa la puerta por la que se realizarán
+	 *               las entradas.
+	 * @param parque una instancia de la interfaz IParque que representa el parque
+	 *               en el que se realizarán las entradas.
+	 */
 	public ActividadSalidaPuerta(String puerta, IParque parque) {
 		this.puerta = puerta;
 		this.parque = parque;
 	}
 
+	/**
+	 * Implementación del método run() de la interfaz Runnable. Realiza la actividad
+	 * de entrada al parque a través de la puerta especificada. Se realizan 20
+	 * entradas, con un tiempo de espera aleatorio entre 0 y 5 segundos entre cada
+	 * entrada. Si la actividad es interrumpida, se registra un mensaje de log
+	 * indicando la interrupción y la excepción lanzada.
+	 */
 	@Override
 	public void run() {
 		for (int i = 0; i < NUMENTRADAS; i++) {
